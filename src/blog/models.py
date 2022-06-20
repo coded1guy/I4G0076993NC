c3bc8527aba1dbc86d1a2358e35f2bde2c917ab6
coded1guy <grggmmnl@gmail.com>
2022-06-20 09:19:39 +0100

    update

---
+++ src/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    Published_date = models.DateTimeField(auto_now_add=True)
+    
